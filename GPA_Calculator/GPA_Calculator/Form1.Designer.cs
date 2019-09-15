a4c79aab991f76417492597edb155235f6cf3b0b
JeremyPyles <41521446+JeremyPyles@users.noreply.github.com>
2019-09-15 19:53:35 -0400

    Upload GPA Calculator Project

---
+++ GPA_Calculator/GPA_Calculator/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace GPA_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblCredits = new System.Windows.Forms.Label();
+            this.lblClassName = new System.Windows.Forms.Label();
+            this.lblGrade = new System.Windows.Forms.Label();
+            this.btnAddAnother = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnDeleteComboBox = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnCalculate.AutoSize = true;
+            this.btnCalculate.Location = new System.Drawing.Point(304, 742);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(156, 46);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblCredits
+            // 
+            this.lblCredits.AutoSize = true;
+            this.lblCredits.Location = new System.Drawing.Point(635, 83);
+            this.lblCredits.Name = "lblCredits";
+            this.lblCredits.Size = new System.Drawing.Size(59, 20);
+            this.lblCredits.TabIndex = 3;
+            this.lblCredits.Text = "Credits";
+            // 
+            // lblClassName
+            // 
+            this.lblClassName.AutoSize = true;
+            this.lblClassName.Location = new System.Drawing.Point(226, 83);
+            this.lblClassName.Name = "lblClassName";
+            this.lblClassName.Size = new System.Drawing.Size(94, 20);
+            this.lblClassName.TabIndex = 4;
+            this.lblClassName.Text = "Class Name";
+            // 
+            // lblGrade
+            // 
+            this.lblGrade.AutoSize = true;
+            this.lblGrade.Location = new System.Drawing.Point(747, 83);
+            this.lblGrade.Name = "lblGrade";
+            this.lblGrade.Size = new System.Drawing.Size(100, 20);
+            this.lblGrade.TabIndex = 5;
+            this.lblGrade.Text = "Letter Grade";
+            // 
+            // btnAddAnother
+            // 
+            this.btnAddAnother.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnAddAnother.Location = new System.Drawing.Point(487, 742);
+            this.btnAddAnother.Name = "btnAddAnother";
+            this.btnAddAnother.Size = new System.Drawing.Size(166, 46);
+            this.btnAddAnother.TabIndex = 10;
+            this.btnAddAnother.TabStop = false;
+            this.btnAddAnother.Text = "Add Another Class";
+            this.btnAddAnother.UseVisualStyleBackColor = true;
+            this.btnAddAnother.Click += new System.EventHandler(this.btnAddAnother_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(565, 132);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(564, 543);
+            this.panel1.TabIndex = 11;
+            // 
+            // btnDeleteComboBox
+            // 
+            this.btnDeleteComboBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnDeleteComboBox.Location = new System.Drawing.Point(682, 742);
+            this.btnDeleteComboBox.Name = "btnDeleteComboBox";
+            this.btnDeleteComboBox.Size = new System.Drawing.Size(153, 46);
+            this.btnDeleteComboBox.TabIndex = 12;
+            this.btnDeleteComboBox.Text = "Delete Course";
+            this.btnDeleteComboBox.UseVisualStyleBackColor = true;
+            this.btnDeleteComboBox.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(120, 132);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(384, 543);
+            this.panel2.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1141, 800);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.btnDeleteComboBox);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnAddAnother);
+            this.Controls.Add(this.lblGrade);
+            this.Controls.Add(this.lblClassName);
+            this.Controls.Add(this.lblCredits);
+            this.Controls.Add(this.btnCalculate);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "GPA Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblCredits;
+        private System.Windows.Forms.Label lblClassName;
+        private System.Windows.Forms.Label lblGrade;
+        private System.Windows.Forms.Button btnAddAnother;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnDeleteComboBox;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ GPA_Calculator/GPA_Calculator/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GPA_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public double getGPA()
+        {
+            double gpa = 0;
+            double grade = 0;
+            double totalCredits = 0;
+            List<String> letterGrade = new List<String>();
+            List<String> numCredits = new List<String>();
+
+            foreach (Control c in panel1.Controls.Cast<Control>())
+            {
+                if (c.GetType().ToString() == "System.Windows.Forms.TextBox")
+                {
+                    if (c.Text == "")
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        numCredits.Add(c.Text);
+
+                    }
+                }
+                else if (c.GetType().ToString() == "System.Windows.Forms.ComboBox")
+                {
+                    if (c.Text == "")
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        letterGrade.Add(c.Text);
+                    }
+                }
+            }
+
+            if (letterGrade.Count != numCredits.Count)
+            {
+                return -1;
+            }
+
+            Double temp;
+
+            for (int i = 0; i < letterGrade.Count; i++)
+            {
+                try
+                {
+                    temp = Convert.ToDouble(numCredits[i]);
+                }
+                catch (System.FormatException)
+                {
+                    return -2;
+                }
+                switch (letterGrade[i])
+                {
+                    case "A+":
+                    case "A": grade += 4.0 * Convert.ToDouble(numCredits[i]); break;
+                    case "A-": grade += 3.7 * Convert.ToDouble(numCredits[i]); break;
+                    case "B+": grade += 3.3 * Convert.ToDouble(numCredits[i]); break;
+                    case "B": grade += 3 * Convert.ToDouble(numCredits[i]); break;
+                    case "B-": grade += 2.7 * Convert.ToDouble(numCredits[i]); break;
+                    case "C+": grade += 2.3 * Convert.ToDouble(numCredits[i]); break;
+                    case "C": grade += 2 * Convert.ToDouble(numCredits[i]); break;
+                    case "C-": grade += 1.7 * Convert.ToDouble(numCredits[i]); break;
+                    case "D+": grade += 1.3 * Convert.ToDouble(numCredits[i]); break;
+                    case "D": grade += 1.0 * Convert.ToDouble(numCredits[i]); break;
+                    case "D-": grade += 0.7 * Convert.ToDouble(numCredits[i]); break;
+                    default: break;
+                }
+                totalCredits += Convert.ToDouble(numCredits[i]);
+            }
+
+            return grade / totalCredits;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double gpa = getGPA();
+            if (gpa == -1)
+            {
+                MessageBox.Show("Please fill in all the boxes");
+            }
+            else if (gpa == -2)
+            {
+                MessageBox.Show("Incorrect Input");
+            }
+            else if (gpa % 1 == 0)
+            {
+                MessageBox.Show(Convert.ToString(gpa) + ".0", "Your GPA");
+
+            }
+            else
+            {
+                MessageBox.Show(Convert.ToString(gpa), "Your GPA");
+            }
+        }
+
+        private void btnAddAnother_Click(object sender, EventArgs e)
+        {
+            //create textbox in panel
+            TextBox textbox = new TextBox();
+            int count = panel1.Controls.OfType<TextBox>().ToList().Count;
+            if (count == 10)
+            {
+                return;
+            }
+            textbox.Location = new System.Drawing.Point(36, 35 * count);
+            textbox.Size = new System.Drawing.Size(58, 25);
+            textbox.Name = "txt_" + (count + 1); 
+            textbox.TextAlign = HorizontalAlignment.Center;
+            panel1.Controls.Add(textbox);
+
+            //create combobox
+            ComboBox combobox = new ComboBox();
+            combobox.Location = new System.Drawing.Point(122, 35 * count);
+            combobox.Size = new System.Drawing.Size(58, 25);
+            combobox.Name = "cmb_" + (count + 1);
+            combobox.DisplayMember = "Name";
+            combobox.ValueMember = "Value";
+            combobox.Items.AddRange(new[]
+            {
+                new GradeOptions() {Name = "Select", Value = "", Selectable = false },
+                new GradeOptions() {Name = "A+", Value = "A+", Selectable = true},
+                new GradeOptions() {Name = "A", Value = "A", Selectable = true},
+                new GradeOptions() {Name = "A-", Value = "A-", Selectable = true},
+                new GradeOptions() {Name = "B+", Value = "B+", Selectable = true},
+                new GradeOptions() {Name = "B", Value = "B", Selectable = true},
+                new GradeOptions() {Name = "B-", Value = "B-", Selectable = true},
+                new GradeOptions() {Name = "C+", Value = "C+", Selectable = true},
+                new GradeOptions() {Name = "C", Value = "C", Selectable = true},
+                new GradeOptions() {Name = "C-", Value = "C-", Selectable = true},
+                new GradeOptions() {Name = "D+", Value = "D+", Selectable = true},
+                new GradeOptions() {Name = "D", Value = "D", Selectable = true},
+                new GradeOptions() {Name = "D-", Value = "D-", Selectable = true},
+                new GradeOptions() {Name = "F", Value = "F", Selectable = true},
+            });
+            combobox.SelectedIndex = 0;
+            combobox.Anchor = (AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right);
+            combobox.DropDownStyle = ComboBoxStyle.DropDownList;
+            panel1.Controls.Add(combobox);
+
+            //create textbox in form
+            TextBox textbox2 = new TextBox();
+            int count2 = panel2.Controls.OfType<TextBox>().ToList().Count;
+            textbox2.Location = new System.Drawing.Point(40, 35 * count2);
+            textbox2.Size = new System.Drawing.Size(125, 25);
+            textbox2.Name = "txt2_" + (count + 1);
+            textbox2.TextAlign = HorizontalAlignment.Left;
+            panel2.Controls.Add(textbox2);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            if (panel1.Controls.Count == 0 || panel2.Controls.Count == 0)
+            {
+                return;
+            }
+            panel1.Controls.RemoveAt(panel1.Controls.Count - 1);
+            panel1.Controls.RemoveAt(panel1.Controls.Count - 1);
+            panel2.Controls.RemoveAt(panel2.Controls.Count - 1);
+        }
+    }
+
+    public class GradeOptions
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public bool Selectable { get; set; }
+    }
+}
